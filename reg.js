31a803b4b76dc6e940eb1a7336a2c27e2a4c96ff
Sophie Neuenmuller <sarisiaice@gmail.com>
2018-11-17 01:38:33 +0900

    reg: fix styling

--- reg.js
+++ reg.js
@@ -3,14 +3,14 @@ const regedit = require('regedit')
 const addr = 'HKCU\\Software\\GameOn\\Pmang\\tera'
 
 module.exports = {
-    getLocation: () => {
+    getLocation() {
         regedit.list(addr, (err, res) => {
             const loc = res[addr].values['location'].value
             return loc ? loc : null
         })
     },
     
-    setLocation: location => {
+    setLocation(location) {
         const toPut = {
             value: location,
             type: 'REG_SZ'
